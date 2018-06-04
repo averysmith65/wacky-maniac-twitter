ced4c9caa26870d2402b2c83c8f38f4be778d94c
Javier Ruedas <jruedas@twc.com>
2018-06-04 17:27:38 -0500

    implement model tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,4 +5,20 @@ RSpec.describe User, type: :model do
   	user = User.new(email: 'jojo@mojo.com', password: 'xnsaadlahdh98lkjad', name: 'Mojo Jojo', username: 'mojojojo', bio: 'crazy evil genius monkey', location: 'Townsville')
   	expect(user).to be_valid
   end
+
+  it 'is invalid without a username' do
+  	user = User.new(email: 'jojo@mojo.com', password: 'xnsaadlahdh98lkjad', name: 'Mojo Jojo', username: nil, bio: 'crazy evil genius monkey', location: 'Townsville')
+		expect(user).not_to be_valid
+	end
+
+	it 'is invalid without an email' do
+  	user = User.new(email: nil, password: 'xnsaadlahdh98lkjad', name: 'Mojo Jojo', username: 'mojojojo', bio: 'crazy evil genius monkey', location: 'Townsville')
+		expect(user).not_to be_valid
+	end
+
+	it 'is invalid without a password' do
+		user = User.new(email: 'jojo@jojo.com', password: nil, name: 'Mojo Jojo', username: 'mojojojo', bio: 'crazy evil genius monkey', location: 'Townsville')
+		expect(user).not_to be_valid
+	end
+
 end
