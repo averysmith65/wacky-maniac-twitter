6e45cee8fc2af53a25cedaa3cac26dc64395d579
Javier Ruedas <jruedas@twc.com>
2018-06-05 16:52:33 -0500

    implement model tests

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,17 @@
 FactoryBot.define do
   factory :user do
+  	email {FFaker::Internet.email}
+  	name {FFaker::NameMX.full_name}
+  	password 'password'
+  	location {FFaker::Address.city}
+  	bio {FFaker::Lorem.sentences}
+  	username {FFaker::NameMX.name}
+
+  	after(:build) do |user|
+  		[:goodmorning, :goodevening].each do |tweet|
+  			user.tweets << FactoryBot.build(:goodevening, user: user)
+  		end	
+  	end
     
   end
 end
--- spec/models/tweet_spec.rb
+++ spec/models/tweet_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Tweet, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'has a valid factory' do
+  	expect(FactoryBot.build(:goodmorning)).to be_valid
+  end
+
+  it 'is valid with required attributes' do
+  	tweet = create(:goodmorning)
+  	expect(tweet).to be_valid
+  end
+
+  it 'is invalid without a message' do
+  	tweet = build(:goodevening, message: nil)
+  	expect(tweet).not_to be_valid
+  end
+
+  it 'is invalid without a user_id' do
+  	tweet = build(:goodmorning, user_id: nil)
+  	expect(tweet).not_to be_valid
+  end
+
 end
