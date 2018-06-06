564c6faec0e1f369bee0fcb0b08553d2e37d73e2
Javier Ruedas <jruedas@twc.com>
2018-06-06 09:27:39 -0500

    changes to tweets controller

--- spec/controllers/tweets_controller_spec.rb
+++ spec/controllers/tweets_controller_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe TweetsController, type: :controller do
 
 	include Devise::Test::ControllerHelpers
-	let(:user) { build(:user) }
+	#let(:user) { build(:user) }
 	describe "GET #index" do
 		it 'renders the tweets index template' do
       user = create(:user)
