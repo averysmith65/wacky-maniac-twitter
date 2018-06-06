703338819bf9af7cd0626e45edf6c0073aaf5549
Javier Ruedas <jruedas@twc.com>
2018-06-06 15:34:04 -0500

    write controller tests and remove auto-generated tests to tmp dir

--- spec/controllers/tweets_controller_spec.rb
+++ spec/controllers/tweets_controller_spec.rb
@@ -3,17 +3,62 @@ require 'rails_helper'
 RSpec.describe TweetsController, type: :controller do
 
 	include Devise::Test::ControllerHelpers
-	let(:user) { build(:user) }
-	describe "GET #index" do
+	
+  #let(:user) { build(:user) }
+  before :each do
+    user = create(:user)
+    sign_in user
+  end
+
+  describe "GET #index" do
 		it 'renders the tweets index template' do
-      user = create(:user)
-      sign_in user
       get :index
       expect(response).to render_template(:index)
       #response represents the finished product from our controller action
     end
 
-
+    it 'returns all tweets' do
+      get :index
+      expect(assigns(:tweets)).not_to be_nil
+    end
 	end
+
+  describe "GET #show" do
+    it 'assigns the show template' do
+      tweet = create(:goodmorning)
+      get :show, params: { id: tweet.to_param }
+      expect(response).to render_template(:show)
+    end
+
+    it 'assigns the requested tweet as @tweet' do
+      tweet = create(:goodevening)
+      get :show, params: { id: tweet.to_param }
+      expect(assigns(:tweet)).to eq(tweet)
+    end
+  end
+
+  describe 'GET #new' do
+    it 'renders the new template' do
+      get :new
+      expect(response).to render_template(:new)
+    end
+    it 'assigns a new tweet to @tweet' do
+      get :new
+      expect(assigns(:tweet)).to be_a_new(Tweet)
+    end
+  end
+
+  describe 'GET #edit' do
+    it 'assigns the requested tweet as @tweet' do
+      tweet = create(:goodmorning)
+      get :edit, params: { id: tweet.to_param }
+      expect(assigns(:tweet)).to eq(tweet)
+    end
+    it 'assigns the edit template' do
+      tweet = create(:goodevening)
+      get :edit, params: { id: tweet.to_param }
+      expect(response).to render_template(:edit)
+    end
+  end
 
 end
