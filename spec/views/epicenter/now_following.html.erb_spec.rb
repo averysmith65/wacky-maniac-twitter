f2438b4578d3e0d5abccab59610064345fc03329
Javier Ruedas <jruedas@twc.com>
2018-06-11 09:47:45 -0500

    remove auto-generated tests

--- spec/views/epicenter/now_following.html.erb_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe "epicenter/now_following.html.erb", type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
--- spec/views/epicenter/unfollow.html.erb_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe "epicenter/unfollow.html.erb", type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
