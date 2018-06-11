a36e2906f00174e04df61cea3c949390e21c8f3a
Javier Ruedas <jruedas@twc.com>
2018-06-11 09:43:26 -0500

    finish epicenter

--- app/controllers/epicenter_controller.rb
+++ app/controllers/epicenter_controller.rb
@@ -16,8 +16,20 @@ class EpicenterController < ApplicationController
 	end
 
   def now_following
-  end
+	  # We are adding the user.id of the user you want to 
+	  # follow to your following array.
+	  # and we want to make sure it's saved in there as an integer
+	  current_user.following.push(params[:id].to_i)
+	  current_user.save
+
+	  redirect_to show_user_path(id: params[:id])
+	end
 
   def unfollow
-  end
+	  current_user.following.delete(params[:id].to_i)
+	  current_user.save
+
+	  redirect_to show_user_path(id: params[:id])  	
+	end
+
 end
